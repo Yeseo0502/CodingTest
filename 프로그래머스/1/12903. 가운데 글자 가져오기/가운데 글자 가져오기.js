145526c4fed0566b61fa7e90625ab231be1d6834
정예서 <5252yeseo@gmail.com>
2025-10-15 23:42:37 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
@@ -0,0 +1,7 @@
+function solution(s) {
+    var answer = '';
+    let mid = Math.floor(s.length/2);
+    if(s.length % 2 === 1) answer=s.substr(mid,1);
+    else answer = s.substr(mid-1,2);
+    return answer;
+}
