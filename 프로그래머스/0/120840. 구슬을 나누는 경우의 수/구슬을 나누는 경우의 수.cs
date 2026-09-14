f7d443cd8d29ae40eee25755d60a07f09a436cbf
정예서 <5252yeseo@gmail.com>
2026-09-14 09:53:31 +0900

    [level 0] Title: 구슬을 나누는 경우의 수, Time: 0.16 ms, Memory: 45.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.cs
@@ -0,0 +1,18 @@
+using System;
+
+public class Solution
+{
+    public int solution(int balls, int share)
+    {
+        long answer = 1;
+
+        share = Math.Min(share, balls - share);
+
+        for (int i = 1; i <= share; i++)
+        {
+            answer = answer * (balls - i + 1) / i;
+        }
+
+        return (int)answer;
+    }
+}
